15e46e5b04e926bebd12af105ddcaa12c867ddb0
AsadPug <97375660+AsadPug@users.noreply.github.com>
2023-04-25 11:32:29 -0400

    updated table
    SERIAL -> INTEGER

--- creation_AbigailF.sql
+++ creation_AbigailF.sql
@@ -1,18 +1,20 @@
+DROP TABLE IF EXISTS inspection, vehicule_inspection, profileur_laser_inspection, troncon_inspection;
+
 CREATE TABLE inspection(
 	id					SERIAL,
 	date_debut 			DATE		NOT NULL,
 	date_fin			DATE		NOT NULL,
-	id_veh_insp			SERIAL		NOT NULL,
-	id_prof_laser_insp	SERIAL		NOT NULL,
-	chemin_fichier 		CHAR(1024)	NOT NULL,
+	veh_insp			INTEGER		NOT NULL,
+	prof_laser_insp		INTEGER		NOT NULL,
+	chemin_fichier 		VARCHAR(1024)NOT NULL,
 	
 	CONSTRAINT pk_ins PRIMARY KEY(id)
 );
 
 CREATE TABLE vehicule_inspection(
 	id 					SERIAL,
-	id_employe			SERIAL 		NOT NULL,
-	id_vehicule			SERIAL		NOT NULL,
+	employe				INTEGER 	NOT NULL,
+	vehicule			INTEGER		NOT NULL,
 	kilo_debut			INTEGER		NOT NULL,
 	kilo_fin			INTEGER		NOT NULL,
 	
@@ -27,16 +29,16 @@ CREATE TABLE vehicule_inspection(
 
 CREATE TABLE profileur_laser_inspection(
 	id 					SERIAL,
-	id_profileur		SERIAL 		NOT NULL,
-	id_employe			SERIAL 		NOT NULL,
+	profileur			INTEGER 	NOT NULL,
+	employe				INTEGER 	NOT NULL,
 	
 	CONSTRAINT pk_pro_ins PRIMARY KEY(id)
 );
 
 CREATE TABLE troncon_inspection(
 	id					SERIAL,
-	id_troncon			SERIAL		NOT NULL,
-	id_inspection 		SERIAL 		NOT NULL,
+	troncon				INTEGER		NOT NULL,
+	inspection 			INTEGER 	NOT NULL,
 	
 	CONSTRAINT pk_tro_ins PRIMARY KEY(id)
 );
