804792c8e0a38d06f5f7bc79055304cc7dca3085
TotoGarneau <98351865+TotoGarneau@users.noreply.github.com>
2023-04-28 10:27:34 -0400

    Update insertions.sql

--- insertions.sql
+++ insertions.sql
@@ -6,3 +6,29 @@ VALUES('accès fauteuil roulant');
 --Abigail
 INSERT INTO type_dispositif_particulier(type)
 VALUES('signal audio pour piéton');
+
+--Thomas
+INSERT INTO forme (forme)
+  VALUES("ronde"),
+        ("carrée"),
+        ("losange"),
+        ("flèche haut"),
+        ("flèche droite"),
+        ("flèche gauche"),
+        ("flèche bas"),
+        ("flèche haut-droite"),
+        ("flèche haut-gauche"),
+        ("flèche bas-droite"),
+        ("flèche bas-gauche"),
+        ("humain"),
+        ("main"),
+        ("vélo"),
+        ("barre verticale"),
+        ("barre horizontale");
+        
+--Thomas
+INSERT INTO couleur (hex, nom)
+  VALUES("FF0000", "rouge"),
+        ("FFFF00", "jaune"),
+        ("00FF00", "vert"),
+        ("FFFFFF", "blanc");
