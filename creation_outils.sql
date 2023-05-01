4397905c342de6d33ebfd00978a148b06dccf06f
Kerian Devillers <98911750+Thelazerlog@users.noreply.github.com>
2023-05-01 07:58:55 -0400

    Update creation_outils.sql

--- creation_outils.sql
+++ creation_outils.sql
@@ -1,1 +1,9 @@
-
+CREATE OR REPLACE PROCEDURE insert_inter(
+	coordonees_inter intersection.coordonees%TYPE,
+	pavage_inter intersection.pavage%TYPE
+)
+LANGUAGE SQL
+AS $$
+	INSERT INTO intersection(identifiant, coordonees, pavage)
+		VALUES (NEXTVAL('identifiant_intersection'), coordonees_inter, pavage_inter);
+$$;
