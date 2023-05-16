368c433698de6d3e58b1bda8bd1b8114989a9a62
TotoGarneau <98351865+TotoGarneau@users.noreply.github.com>
2023-05-16 10:58:12 -0400

    Update creation_structure.sql

--- creation_structure.sql
+++ creation_structure.sql
@@ -35,6 +35,7 @@ DROP TABLE IF EXISTS couleur;
 DROP TABLE IF EXISTS forme;
 DROP TABLE IF EXISTS lumiere;
 DROP TABLE IF EXISTS dispositif_lumineux;
+DROP TABLE IF EXISTS vehicule, calibration, profileur_laser, employe, poste, departement, troncon, intersection, inspection, troncon_inspection; 
 
 -- Suppression de enums
 
@@ -42,8 +43,6 @@ DROP TYPE IF EXISTS TYPE_MODE CASCADE;
 DROP TYPE IF EXISTS TYPE_ORIENTAION CASCADE;
 DROP TYPE IF EXISTS genre CASCADE;
 DROP TYPE IF EXISTS pavage CASCADE;
-
-DROP TABLE IF EXISTS vehicule, calibration, profileur_laser, employe, poste, departement, troncon, intersection, inspection, troncon_inspection; 
 
 --Suppression sequences
 DROP SEQUENCE IF EXISTS identifiant_intersection;
